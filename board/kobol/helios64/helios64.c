0cd5dbd9295020674615712b83466d76ceb297de
xoddark <xod@xoddark.com>
2021-11-05 18:36:15 +0100

    fix for v2020.10 change

--- board/kobol/helios64/helios64.c
+++ board/kobol/helios64/helios64.c
@@ -281,7 +281,7 @@ int checkboard(void)
 #endif
 
 #if defined(CONFIG_OF_LIBFDT) && defined(CONFIG_OF_BOARD_SETUP)
-int ft_board_setup(void *blob, bd_t *bd)
+int ft_board_setup(void *blob, struct bd_info *bd)
 {
 	char *env;
 
